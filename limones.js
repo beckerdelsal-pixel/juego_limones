dba5362ec21e48826ebba2b97449dc9127a321a5
beckerdelsal-pixel <beckerdelsal@gmail.com>
2026-09-18 15:35:59 -0500

    mover a la izquierda

--- limones.js
+++ limones.js
@@ -5,6 +5,8 @@ const ALTURA_SUELO = 20
 const ALTURA_PERSONAJE =60;
 const ANCHO_PERSONAJE = 40;
 
+let personajeX=canvas.width/2;
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -17,5 +19,22 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="#ebe048"
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajeX = personajeX-10
+    actualizarPantalla();
+}
+
+
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
 }
